82d2d6db1760dc17b523addfe4ed8aaf9849b01e
Alshifa Belim <belimal@sheridancollege.ca>
2024-09-17 09:42:08 -0400

    Part time student2 added in new branch

---
+++ OneDrive/Documents/NetBeansProjects/StudentList/src/studentlist/PartTimeStudent2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author belim
+ */
+public class PartTimeStudent2 extends Student {
+    private int numofcourses2;
+
+    public int getNumofcourses2() {
+        return numofcourses2;
+    }
+
+    public void setNumofcourses2(int numofcourses) {
+        this.numofcourses2 = numofcourses;
+    }
+}
