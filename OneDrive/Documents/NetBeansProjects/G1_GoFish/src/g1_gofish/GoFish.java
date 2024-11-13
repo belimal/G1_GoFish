6cf406257652089cd3eb1ad84294f4dc6c7517e3
Carlo Maximo <maximoc@sheridancollege.ca>
2024-11-12 19:48:15 -0500

    Initial commit; redid game logic to accommodate one human and one computer player.

---
+++ OneDrive/Documents/NetBeansProjects/G1_GoFish/src/g1_gofish/GoFish.java
@@ -0,0 +1,12 @@
+// @author: Alshifa Belim, Carlo Maximo, Palakpreet Kaur
+package g1_gofish;
+
+// Main class
+
+import java.util.Scanner;
+
+public class GoFish {
+    public static void main(String[] args) {
+        Game game = new Game();
+    }
+}
